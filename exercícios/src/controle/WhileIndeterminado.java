d5dc0888ae5603be244f4b94b2eaba16947d0d6d
Leandro Pereira <leandro.nares@gmail.com>
2019-10-06 12:40:28 -0300

    64. DO/WHILE

--- exercícios/src/controle/WhileIndeterminado.java
+++ exercícios/src/controle/WhileIndeterminado.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class WhileIndeterminado {
 
 	public static void main(String[] args) {
-		
+
 		Scanner entrada = new Scanner(System.in);
-		
+
 		String valor = "";
-		
-		while(!valor.equalsIgnoreCase("sair")) {
+
+		while (!valor.equalsIgnoreCase("sair")) {
 			System.out.println("Você diz: ");
 			valor = entrada.nextLine();
 		}
-		
+
 		entrada.close();
 
 	}
