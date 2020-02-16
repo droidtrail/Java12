9a682154655f1895e8d8fcb5dc2455ecfc4e90d0
Leandro Pereira <leandro.nares@gmail.com>
2020-02-15 23:28:45 -0300

    123. List

--- exercícios/src/Colecoes/ConjuntoComportado.java
+++ exercícios/src/Colecoes/ConjuntoComportado.java
@@ -7,26 +7,25 @@ public class ConjuntoComportado {
 
 	public static void main(String[] args) {
 
-		//Set<String> listaAprovados = new HashSet<>();
+		// Set<String> listaAprovados = new HashSet<>();
 		SortedSet<String> listaAprovados = new TreeSet<>();
-			listaAprovados.add("Ana");
-			listaAprovados.add("Luca");
-			listaAprovados.add("Carlos");
-			listaAprovados.add("Pedro");
-		
-		for(String candidato: listaAprovados) {
+		listaAprovados.add("Ana");
+		listaAprovados.add("Luca");
+		listaAprovados.add("Carlos");
+		listaAprovados.add("Pedro");
+
+		for (String candidato : listaAprovados) {
 			System.out.println(candidato);
 		}
-		
-		SortedSet<Integer> nums = new TreeSet<>();
-			nums.add(1);
-			nums.add(2);
-			nums.add(120);
-			nums.add(6);
-			
-			for(int n:nums) {
-				System.out.println(n);
-			}
-	}
 
+		SortedSet<Integer> nums = new TreeSet<>();
+		nums.add(1);
+		nums.add(2);
+		nums.add(120);
+		nums.add(6);
+
+		for (int n : nums) {
+			System.out.println(n);
+		}
+	}
 }
