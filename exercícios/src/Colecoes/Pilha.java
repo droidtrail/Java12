e845a73c9b5aa76db84e907364a8ecce7a9335b9
Leandro Pereira <leandro.nares@gmail.com>
2020-03-14 09:55:07 -0300

    125.Stack(Pilha)

---
+++ exercícios/src/Colecoes/Pilha.java
@@ -0,0 +1,41 @@
+package Colecoes;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Pilha {
+	
+	public static void main(String[] args) {
+		
+		Deque<String> livros = new ArrayDeque<String>();
+		
+		livros.add("O Pequeno Príncipe");
+		livros.push("Don Quixote");
+		livros.push("O Hobbit");
+		
+		for(String livro: livros) {
+			System.out.println(livro);
+		}
+		
+		//Exibindo os valores
+		System.out.println(livros.peek());
+		System.out.println(livros.element());
+		
+		/*
+			System.out.println(livros.pop());
+			System.out.println(livros.poll());
+			System.out.println(livros.poll());
+			System.out.println(livros.poll());
+			System.out.println(livros.poll());
+			System.out.println(livros.pop());
+			System.out.println(livros.remove());
+		*/
+		
+		livros.size();
+		livros.clear();
+		livros.contains(livros);
+		livros.isEmpty();
+		
+	}
+
+}
